94698b7deae8cf3d96fac20b86146d9f6c2afa2f
Pallavi Pattanashetti <Pallavi.Pattanashetti@gmail.com>
2026-03-25 20:51:36 -0700

    done

--- interfaces/interfaces.ts
+++ interfaces/interfaces.ts
@@ -1,19 +1,13 @@
-export interface Pokemon {
-    id:number
-    name: string
-    img: string
-    shinyImg:string
-    moves: string
- location: string
-    type:string
-    abilities:string
-    evolution:string
-    habitat:string
-
+export interface PokemonData {
+  id: number;
+  name: string;
+  img: string;
+  shinyImg: string;
+  type: string;
+  abilities: string;
+  moves: string;
+  location: string;
+  evolution: string;
 }
 
-export interface PokemonContextType {
-    pokemon: Pokemon | null;
-    setPokemon: (pokemon: Pokemon | null) => void
 
-}
